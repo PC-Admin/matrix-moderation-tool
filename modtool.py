93b17b52131232d3fddd6d203bcce87f0e61596e
PC-Admin <perthchat@protonmail.com>
2023-07-08 06:18:44 +0800

    update default tags for rdlist and cleanup readme

--- modtool.py
+++ modtool.py
@@ -30,7 +30,7 @@ base_url = "example.org"
 access_token = ""
 # rdlist specific
 rdlist_bot_username = "mod_team"
-rdlist_recommended_tags = ['hub_room_links', 'preban', 'degen_misc', 'beastiality', 'degen_porn', 'gore', 'hub_room_trade', 'snuff', 'degen_larp', 'hub_room_sussy', 'bot_spam cfm', '3d_loli', 'jailbait', 'bot_porn', 'toddlercon', 'loli', 'csam', 'tfm', 'abandoned', 'degen_meet', 'stylized_3d_loli']
+rdlist_recommended_tags = ['hub_room_links', 'hub_room_trade', 'preban', 'degen_misc', 'beastiality', 'degen_porn', 'gore', 'snuff', 'degen_larp', 'hub_room_sussy', 'bot_spam', 'cfm', 'jailbait', 'bot_porn', 'toddlercon', 'loli', 'csam', 'tfm', 'degen_meet', 'stylized_3d_loli', '3d_loli']
 ###########################################################################
 
 def parse_username(username):
