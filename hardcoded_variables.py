da1322dc8dd7a012cd1b103be25c2e2caadcf4ec
PC-Admin <perthchat@protonmail.com>
2023-07-10 04:59:30 +0800

    re-add vars file (whoops)

---
+++ hardcoded_variables.py
@@ -0,0 +1,9 @@
+
+###########################################################################
+# These values can be hard coded for easier usage:                        #
+homeserver_url = "matrix.example.org"
+base_url = "example.org"
+access_token = ""
+# rdlist specific
+rdlist_bot_username = "mod_team"
+###########################################################################
